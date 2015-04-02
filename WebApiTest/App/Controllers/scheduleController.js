0d0707d821a75955df939637dbdf1e33b95963bf
Akanksha <akanksha@e10.in>
2015-04-02 17:29:08 +0530

    Angular js files added

---
+++ WebApiTest/App/Controllers/scheduleController.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('scheduleApp')
+        .controller('scheduleController', scheduleController);
+
+    scheduleController.$inject = ['$scope', 'Schedules'];
+
+    function moviesController($scope, Schedules) {
+        $scope.schedules = Schedules.query();
+    }
+})();
---
+++ WebApiTest/App/scheduleServices.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('scheduleServices', ['ngResource']);
+
+    app.factory('Schedule', ['$resource',
+      function ($resource) {
+          return $resource('/api/schedule/', {}, {
+              query: { method: 'GET', params: {}, isArray: true }
+          });
+      }]);
+
+
+})();
